1318ee1623e26e7344439b77a2e1dca3ebdc5765
shun <wangshun@enhance.cn>
2016-09-13 10:08:16 +0800

    Initial Commit

---
+++ MyMusic_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMusic_OC
+//
+//  Created by shun on 16/9/13.
+//  Copyright © 2016年 shun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMusic_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMusic_OC
+//
+//  Created by shun on 16/9/13.
+//  Copyright © 2016年 shun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
